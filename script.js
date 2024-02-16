2f40ddb22b3fe4b64a5a792b745cc9fc59f3f765
jasmina8835 <sunnatovajasmina8002@gmail.com>
2024-02-16 19:51:40 +0500

    kduhgkelw

---
+++ script.js
@@ -0,0 +1,49 @@
+// Function1
+function doFunc(c, d){
+    return c * d
+}
+
+let b = doFunc(76, 45)
+console.log(b);
+
+// Function2
+function doFunc2(e, f){
+ return e / f
+}
+let c = doFunc2(100, 50)
+console.log(c);
+
+ // Function3
+ function doFunc3(g,h){
+  return g - h
+ }
+ let d = doFunc3(75,30)
+ console.log(d);
+
+//  Function4
+function doFunc4(i,j) {
+   return i ** j
+}
+let e = doFunc4(3,4)
+console.log(e);
+
+// Function5
+function doFunc5(k) {
+  return  Math.sqrt(k)
+}
+let f = doFunc5(144)
+console.log(f);
+
+// Function6
+function doFunc6(l, m) {
+ return l % m
+}
+let g = doFunc6(98,6)
+console.log(g);
+
+// Function7
+function doFunc7(w,y,z,x){
+  return w + y / z * x
+}
+let h = doFunc7(20,50,40,2)
+console.log(h);
